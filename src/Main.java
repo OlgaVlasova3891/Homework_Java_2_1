bacbaa169081107b15f3567d768165abba8d8c44
Olga Vlasova <olgav1918@gmail.com>
2023-03-21 15:06:44 +0300

    initial commoit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 3500; // стоимость авиабилета в рублях
+        int bonus = 20; // начисление бонусных миль на каждые 20 руб стоимости билета
+
+        System.out.println((price/bonus) + " бесплатных бонусов за перелёт");
+
+    }
+}
